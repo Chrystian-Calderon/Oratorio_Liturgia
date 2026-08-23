93cfd0425defeb32bc7bcc6b5dc198fa1fbf5951
Chrystian Calderon <chrystianralfc@gmail.com>
2026-08-22 23:35:04 -0400

    moviendo archivo

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-    const nav = document.querySelector(".navbar-modern");
-
-    if (window.scrollY > 50) {
-        nav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-    }
-});
