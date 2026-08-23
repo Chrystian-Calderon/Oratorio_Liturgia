a259d9cc44c4c55a44e85267f3180e30fde7d3f2
Chrystian Calderon <chrystianralfc@gmail.com>
2026-08-22 23:49:26 -0400

    agregar env

--- servidor/index.php
+++ servidor/index.php
@@ -1,3 +1,10 @@
 <?php
-header("Location: cliente/login.php");
-?>
+
+require_once __DIR__ . '/configuration/env.php';
+
+cargarEnv(__DIR__ . '/../.env');
+
+function env(string $nombre, mixed $default = null): mixed
+{
+    return $_ENV[$nombre] ?? $default;
+}
